c9bff6da99fe8989fe46213fc67ff4c054b45246
Emerson Siega <emerson.siega@ciss.com.br>
2018-09-25 23:48:52 -0300

    fix: Remove unused import

--- src/containers/Bookshelves/__tests__/Bookshelves.js
+++ src/containers/Bookshelves/__tests__/Bookshelves.js
@@ -4,7 +4,7 @@ import React from 'react'
 import Bookshelves from "../../Bookshelves"
 
 // TODO: how to create a setupFile for ezyme in an ejected project?
-import Enzyme, { shallow, mount } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
