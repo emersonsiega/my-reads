b2d930c98ba748ac289f1e1c84399477060a96f1
Emerson Siega <emerson.siega@ciss.com.br>
2018-09-30 19:39:06 -0300

    refactor: Remove unused dependency and use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 import 'typeface-roboto'
@@ -8,8 +8,8 @@ import 'typeface-roboto'
 import App from "./views/App";
 
 ReactDOM.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-    </HashRouter>, 
+    </BrowserRouter>, 
     document.getElementById('root')
 );
